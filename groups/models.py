371c80ca8a5e7e66c01c48db6d19d12b4b6bdc45
beatwas <beatwas.mv@gmail.com>
2022-10-18 15:52:14 +0300

    HW_13

--- groups/models.py
+++ groups/models.py
@@ -1,3 +1,24 @@
-from django.db import models    # noqa
+from datetime import date
 
-# Create your models here.
+from django.core.validators import MinLengthValidator
+from django.db import models
+
+
+class Groups(models.Model):
+    group_name = models.CharField(
+        max_length=100,
+        verbose_name='group name',
+        db_column='group_name',
+        validators=[MinLengthValidator(2, '"group_name" field value less than two symbols')]
+    )
+    date_start = models.DateField(default=date.today, null=True, blank=True)
+    description = models.TextField(
+        max_length=300,
+        verbose_name='description',
+        db_column='description',
+        validators=[MinLengthValidator(2)],
+        error_messages={'min_length': '"description" field value less than two symbols'}
+    )
+
+    def __str__(self):
+        return f'{self.group_name} {self.date_start}\n{self.description}'
